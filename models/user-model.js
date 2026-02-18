0280ac40268cd8105ba026b9d1839e488df53472
Code-Crafter1 <sakshikumarizen@gmail.com>
2026-02-18 09:08:41 +0530

    minor changes

--- models/user-model.js
+++ models/user-model.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     type: Array,
     default: [],
   },
+  isadmin: Boolean,
+  orders: {
+    type: Array,
+    default: [],
+  },
   contact: Number,
   picture: String,
 });
