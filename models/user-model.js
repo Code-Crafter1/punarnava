2f51f9426bd9bec68a93545723ebe960cf4bc8b9
Code-Crafter1 <sakshikumarizen@gmail.com>
2026-02-18 09:06:20 +0530

    user-model initialsetup

---
+++ models/user-model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/punarnava");
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    minlength: 3,
+    trim: true,
+  },
+  email: String,
+  password: String,
+  cart: {
+    type: Array,
+    default: [],
+  },
+  contact: Number,
+  picture: String,
+});
+
+module.exports = mongoose.model("user", userSchema);
