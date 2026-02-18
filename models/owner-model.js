5178c466e77bbe14ca3691508fa544ce1d610d95
Code-Crafter1 <sakshikumarizen@gmail.com>
2026-02-18 11:48:03 +0530

    changes done

---
+++ models/owner-model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const ownerSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    minlength: 3,
+    trim: true,
+  },
+  email: String,
+  password: String,
+  products: {
+    type: Array,
+    default: [],
+  },
+  picture: String,
+  gstin: String,
+});
+
+module.exports = mongoose.model("owner", ownerSchema);
--- models/product-model.js
+++ models/product-model.js
@@ -1,4 +1,3 @@
-const { text } = require("express");
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
